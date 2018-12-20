f3a5598d643bdedf2743ede9a62d79bb2a8d9f61
Fábio Costa <fabio-t-costa@telecom.pt>
2018-12-20 09:15:08 +0000

    keizer always gets one goal against. but still, #keizerball

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-var score = 0;
+var keizerscore = 0;
+var awayscore = 0;
 
 $( function() {
   $("#keizerball").click( function() {
     $(this).animate({ top: "-=100%" }, 400, function() {
       $(this).animate({ top: "+=100%" }, 0);
     });
-    score += 1;
-    $("#scoreboard .keizerscore").text(score);
+    keizerscore += 1;
+    $("#scoreboard .keizerscore").text(keizerscore);
+    if (keizerscore % 3 == 0) {
+      awayscore += 1;
+      $("#scoreboard .awayscore").text(awayscore);
+    }
   })
 });
